9f9648e9ca2318b31dd1e0a5ff5ec9b4e207e025
Matthew Goodyear <mattgdyr@proton.me>
2025-07-27 10:15:39 +0100

    Removed base url from auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,3 @@
 import { createAuthClient } from "better-auth/client";
 
-export const authClient = createAuthClient({
-  baseURL: import.meta.env.BETTER_AUTH_URL,
-  token: import.meta.env.BETTER_AUTH_SECRET,
-});
+export const authClient = createAuthClient({});
